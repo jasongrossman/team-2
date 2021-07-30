23c2cfd85d548a216d78b5d100a785134d7181b7
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-30 16:52:13 -0400

    added API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,63 @@ var selectedCuisine = document.getElementById("cuisine");
 var searchDish = document.getElementById("keyword") //need to create text input field in HTML
 var apiKey = "edb7bd45b42b44c687e56d5221325bf5";
 
-//event handler for cook button
-$("#cook").on("click", function() {
-    //read value of cuisine selection
+    var spoonacularCall = function() {
+    //build API query call to Spoonacular based on dropdown menu value 
     var cuisineQuery = selectedCuisine.value;
-    console.log(cuisineQuery);
-    fetch ("https://api.spoonacular.com/recipes/complexSearch?cuisine=" 
+    fetch("https://api.spoonacular.com/food/wine/pairing" 
+    + "?food=" 
     + cuisineQuery
     + "&apiKey=" + apiKey)
 
     .then(function(response) {
-        // request was successful
-        if (response.ok) {
-          // console.log(response);
-          response.json().then(function(data) {
-            console.log(data);
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function(error) {
-        alert('Unable to connect to Spoonacular');
-      })
+    response.json().then(function(data) {
+    console.log(data)
+
+    .then(function(response) {
+    if (response.data.length === 0) {
+        console.log('Spoonacular could not find anything for that.');    
+
+    } else {
+        console.log(response.data.pairedWines[0])
+    }
+})
+    .catch(function(error) {
+    alert('Unable to connect to Spoonacular');
+    })
+
+    // .then(function(response) {
+    // // request was successful
+    // // if (response.ok) {
+    // // console.log(response);
+    // response.json().then(function(data) {
+    // console.log(data)
+
+    // .then(function(data){ 
+    //     console.log(data);
+    // let cuisineQueryvalue = data['pairedWines'][0];
+    // cuisineQuery.innerHTML = cuisineQueryvalue;
+    // let display = document.createElement("div");
+    // display.appendChild(cuisineQuery);
+    });
+    })
+    // });
+    // } else {
+    // alert('Error: ' + response.statusText);
+    // }
+    // })
+    // .catch(function(error) {
+    // alert('Unable to connect to Spoonacular');
+    // })
+}
+
+    var tastyCall = function() {
+        console.log("ringing tastyCall");
+    }
+
   
 //event handler for search by dish/keyword
-
-
-
+//event handler for cook button
+$("#cook").on("click", function() {
+    spoonacularCall();
+    tastyCall();
 });

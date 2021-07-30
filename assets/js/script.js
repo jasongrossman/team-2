98893a299478f9d044670ace45afe01d0123ae12
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-30 14:57:47 -0400

    added event listener to cook

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//global variable declarations
+var selectedCuisine = document.getElementById("cuisine");
+var searchDish = document.getElementById("keyword") //need to create text input field in HTML
+
+//event handler for cook button
+$("#cook").on("click", function() {
+    //read value of cuisine selection
+    console.log(selectedCuisine.value);
+
+    
+
+//event handler for search by dish/keyword
+
+
+
+});
